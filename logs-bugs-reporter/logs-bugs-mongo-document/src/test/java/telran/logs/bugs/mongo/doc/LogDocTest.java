31bedd9a73fbc310441578fc831f1a15bb350965
Yuliya Dvirniy <yununa@gmail.com>
2021-01-30 08:38:40 +0200

    fixing mongo-document test according Yuri's work

--- logs-bugs-reporter/logs-bugs-mongo-document/src/test/java/telran/logs/bugs/mongo/doc/LogDocTest.java
+++ logs-bugs-reporter/logs-bugs-mongo-document/src/test/java/telran/logs/bugs/mongo/doc/LogDocTest.java
@@ -3,12 +3,8 @@ package telran.logs.bugs.mongo.doc;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
-
 import telran.logs.bugs.dto.*;
-
 import java.util.Date;
-
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.test.context.ContextConfiguration;
@@ -18,7 +14,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes=LogsRepo.class)
 @EnableAutoConfiguration
-//@AutoConfigureDataMongo
 public class LogDocTest {
 	@Autowired
 	LogsRepo logs;
@@ -26,7 +21,7 @@ public class LogDocTest {
 	void docStoreTest() {
 		LogDto logDto = new LogDto(new Date(),LogType.NO_EXCEPTION, "artifact",
 				20,"result");
-		LogDoc doc = logs.save(new LogDoc(logDto));
+		logs.save(new LogDoc(logDto));
 		LogDoc actualDoc = logs.findAll().get(0);
 		assertEquals(logDto, actualDoc.getLogDto());			
 	}
