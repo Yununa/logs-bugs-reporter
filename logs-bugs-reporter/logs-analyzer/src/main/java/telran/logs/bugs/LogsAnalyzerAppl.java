be25b0ef00b0e54c9ccf8fff3c5e38e57624dec5
Yuliya Dvirniy <yununa@gmail.com>
2021-02-03 15:49:29 +0200

    CW-59  StreamBridge for sending objects per binding names

---
+++ logs-bugs-reporter/logs-analyzer/src/main/java/telran/logs/bugs/LogsAnalyzerAppl.java
@@ -0,0 +1,13 @@
+package telran.logs.bugs;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication
+public class LogsAnalyzerAppl {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsAnalyzerAppl.class, args);
+	}
+}
---
+++ logs-bugs-reporter/logs-analyzer/src/test/java/telran/logs/bugs/LogsAnalyzerTest.java
@@ -0,0 +1,52 @@
+package telran.logs.bugs;
+
+import java.util.Date;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+import telran.logs.bugs.dto.*;
+import org.springframework.cloud.stream.binder.test.*;
+
+@SpringBootTest
+@Import(TestChannelBinderConfiguration.class)
+public class LogsAnalyzerTest {
+	static Logger LOG = LoggerFactory.getLogger(LogsAnalyzerTest.class);
+	@Autowired
+	InputDestination producer;
+	@Autowired
+	OutputDestination consumer;
+	@Value("${app-binding-name}")
+	String bindingName;
+	
+	@BeforeEach
+	void setUp() {
+	     consumer.clear();
+	}
+	
+	@Test
+	void analyzerTestNonException() {
+		LogDto logDto = new LogDto(new Date(), LogType.NO_EXCEPTION, "artifact", 0, "result");
+	    sendLogDto(logDto);
+	    assertThrows(Exception.class, consumer::receive);
+	    
+	}
+	
+	@Test
+	void analyzerTestException() {
+		LogDto logDto = new LogDto(new Date(), LogType.AUTHENTICATION_EXCEPTION, "artifact", 0, "result");
+	    sendLogDto(logDto);
+	    Message<byte[]> message = consumer.receive(0, bindingName);
+	    assertNotNull(message);
+	    LOG.debug("receved in consumer {}", new String(message.getPayload()));    
+	}
+	
+	private void sendLogDto(LogDto logDto) {
+		producer.send(new GenericMessage<LogDto>(logDto));
+	}
+}
