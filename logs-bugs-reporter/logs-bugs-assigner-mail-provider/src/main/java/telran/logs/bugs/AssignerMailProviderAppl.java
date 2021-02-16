bbed13d8f7e598c08681c8b0d71ddda016b379d8
Yuliya Dvirniy <yununa@gmail.com>
2021-02-16 11:54:17 +0200

    CW-63 fix HW-62 after code review

--- logs-bugs-reporter/logs-bugs-assigner-mail-provider/src/main/java/telran/logs/bugs/AssignerMailProviderAppl.java
+++ logs-bugs-reporter/logs-bugs-assigner-mail-provider/src/main/java/telran/logs/bugs/AssignerMailProviderAppl.java
@@ -1,5 +1,7 @@
 package telran.logs.bugs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class AssignerMailProviderAppl {
-	
-	@Value("${app-assigner-mail:assigner@gmail.com}")
+	Logger LOG = LoggerFactory.getLogger(AssignerMailProviderAppl.class);
+	@Value("${app-assigner-mail:logs.bugs.reporter+assigner@gmail.com}")
 	String assignerMail;
 
 	public static void main(String[] args) {
@@ -20,7 +22,7 @@ public class AssignerMailProviderAppl {
 	
 	@GetMapping("/mail/assigner")
 	String getAssignerMail() {
-		
+		LOG.debug("\nassigner mail is {}", assignerMail);
 		return assignerMail;
 	}
 }
